d36f50aaac7103abeb8afa04b1b12caa14809ec3
Evan Kaloudis <zeusln@tutanota.com>
2022-11-18 18:15:52 -0500

    demos: remove expo scaffolding in favor of RN cli

--- demos/multi-connect-demo/ios/lncmobiledemo/AppDelegate.h
+++ demos/multi-connect-demo/ios/lncmobiledemo/AppDelegate.h
@@ -1,9 +1,8 @@
-#import <Foundation/Foundation.h>
 #import <React/RCTBridgeDelegate.h>
 #import <UIKit/UIKit.h>
 
-#import <Expo/Expo.h>
+@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
 
-@interface AppDelegate : EXAppDelegateWrapper <RCTBridgeDelegate>
+@property (nonatomic, strong) UIWindow *window;
 
 @end
