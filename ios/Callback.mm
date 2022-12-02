244f61456ca33ec17d2751ad8eeea31084359791
Evan Kaloudis <zeusln@tutanota.com>
2022-12-01 23:05:23 -0500

    ios/Callback.mm: remove unneeded loggers

--- ios/Callback.mm
+++ ios/Callback.mm
@@ -5,13 +5,10 @@
 @implementation Callback
 
 -(void)setCallback:(RCTResponseSenderBlock)callback {
-    NSLog(@"setCallback");
     self.rnCallback = callback;
 }
 
 -(void)sendResult:(NSString *)data {
-    NSLog(@"sendResult");
-    NSLog(data);
     self.rnCallback(@[data]);
 }
 
